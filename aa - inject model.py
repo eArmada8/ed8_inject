e88a4d8025869719fb47712bf28919d71d55df10
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2023-02-07 13:05:01 -0800

    Code overhaul
    The script now properly reads and writes the pkg files, much less of a hack job.  This adds no functionality, but is a needed overhaul for upcoming plan for shader fix script.

--- aa - inject model.py
+++ aa - inject model.py
@@ -2,8 +2,108 @@
 # instead of the existing file.  If no target backup exists, it will create one before erasing the target.
 # GitHub eArmada8/misc_kiseki
 
-import sys, os, shutil, struct
+import sys, os, shutil, struct, io
 from unpackpkg import * # Needed for games that compress the XML file
+
+def get_pkg_contents (f):
+    f.seek(0,0)
+    file_contents = []
+    magic = f.read(4)
+    total_files, = struct.unpack("<I", f.read(4))
+    for i in range(total_files):
+        file = {}
+        file["file_entry_name"], file["file_entry_uncompressed_size"],\
+            file["file_entry_compressed_size"], file["file_entry_offset"],\
+            file["file_entry_flags"] = struct.unpack("<64s4I", f.read(80))
+        file["file_entry_name"] = file["file_entry_name"].rstrip(b"\x00").decode('utf-8')
+        file_contents.append(file)
+    return(file_contents)
+
+def retrieve_file (f, file_entry_name, file_contents, decompress = True):
+    file_entry = [x for x in file_contents if file_entry_name in x["file_entry_name"]]
+    if len(file_entry) > 0:
+        f.seek(file_entry[0]["file_entry_offset"],0)
+        if file_entry[0]["file_entry_flags"] & 0x1 and decompress:
+            return(uncompress_nislzss(f, file_entry[0]["file_entry_uncompressed_size"], file_entry[0]["file_entry_compressed_size"]))
+        elif file_entry[0]["file_entry_flags"] & 0x4 and decompress:
+            return(uncompress_lz4(f, file_entry[0]["file_entry_uncompressed_size"], file_entry[0]["file_entry_compressed_size"]))
+        elif file_entry[0]["file_entry_flags"] & 0x8 and decompress:
+            return(uncompress_zstd(f, file_entry[0]["file_entry_uncompressed_size"], file_entry[0]["file_entry_compressed_size"]))
+        else:
+            return(f.read(file_entry[0]["file_entry_compressed_size"]))
+    else:
+        return False
+
+def retrieve_xml_file (f, file_contents):
+    xml_entry = [x for x in file_contents if 'xml' in x["file_entry_name"]]
+    if len(xml_entry) > 0:
+        return retrieve_file(f, xml_entry[0]["file_entry_name"], file_contents, decompress = True)
+    else:
+        return False
+
+# Input is a file stream of a pkg file
+def retrieve_asset_symbol (f):
+    file_contents = get_pkg_contents(f)
+    xml_file_data = retrieve_xml_file (f, file_contents)
+    asset_symbol_offset = xml_file_data.find(b'asset symbol') + 14
+    rest_of_file_offset = xml_file_data.find(b'"', asset_symbol_offset+1)
+    return(xml_file_data[asset_symbol_offset:rest_of_file_offset].decode('utf-8'))
+
+# Takes binary file data, new symbol as string.  Uses binary search, as parsing XML is overkill.
+def change_xml_asset_symbol (xml_file_data, new_asset_symbol):
+    asset_symbol_offset = xml_file_data.find(b'asset symbol') + 14
+    rest_of_file_offset = xml_file_data.find(b'"', asset_symbol_offset+1)
+    return(xml_file_data[0:asset_symbol_offset] + new_asset_symbol.encode('utf-8') + xml_file_data[rest_of_file_offset:])
+
+# Adds file onto the end of the stream, and appends name/size to contents.  Offsets are not calculated.
+def insert_file_into_stream (f, content_struct, binary_file_data, file_details):
+    f.write(binary_file_data)
+    content_struct.append(file_details)
+    return(content_struct)
+
+# Updates all file offsets in the TOC based on current file size
+def update_file_offsets (content_struct):
+    current_file_offset = len(content_struct) * 80 + 8 # First file offset is always here
+    for i in range(len(content_struct)):
+        content_struct[i]["file_entry_offset"] = current_file_offset
+        current_file_offset += content_struct[i]["file_entry_compressed_size"]
+    return(content_struct)
+
+def write_pkg_file (newfilename, file_stream, content_struct, magic = b'\x00\x00\x00\x00'):
+    # Assume all the file offsets are wrong, and fix them
+    content_struct = update_file_offsets(content_struct)
+    with open(newfilename, 'wb') as f:
+        f.write(magic)
+        f.write(struct.pack("<I", len(content_struct)))
+        for i in range(len(content_struct)):
+            bin_name = content_struct[i]["file_entry_name"]
+            f.write(struct.pack("<64s4I", content_struct[i]["file_entry_name"].encode("utf-8").ljust(64,b'\x00'),\
+                content_struct[i]["file_entry_uncompressed_size"],\
+                content_struct[i]["file_entry_compressed_size"],\
+                content_struct[i]["file_entry_offset"],\
+                content_struct[i]["file_entry_flags"]))
+        file_stream.seek(0)
+        f.write(file_stream.read())
+    return
+
+def inject_asset_symbol_into_pkg(pkg_filename, new_pkg_filename, asset_symbol):
+    with open(pkg_filename, 'rb') as f:
+        file_contents = get_pkg_contents(f)
+        new_file_contents = []
+        new_file_stream = io.BytesIO()
+        for i in range(len(file_contents)):
+            if 'xml' in file_contents[i]["file_entry_name"]:
+                file = retrieve_file (f, file_contents[i]["file_entry_name"], file_contents, decompress = True)
+                file = change_xml_asset_symbol(file, asset_symbol)
+                new_file_contents = insert_file_into_stream (new_file_stream, new_file_contents, file,\
+                    {"file_entry_name": file_contents[i]["file_entry_name"],\
+                    "file_entry_uncompressed_size": len(file),\
+                    "file_entry_compressed_size": len(file),\
+                    "file_entry_offset": 0, "file_entry_flags": 0}) # Offset will be fixed at time of packing
+            else:
+                file = retrieve_file (f, file_contents[i]["file_entry_name"], file_contents, decompress = False)
+                new_file_contents = insert_file_into_stream (new_file_stream, new_file_contents, file, file_contents[i])
+    write_pkg_file (new_pkg_filename, new_file_stream, new_file_contents, magic = b'\x00\x00\x00\x00')
 
 if __name__ == "__main__":
     # Set current directory
@@ -64,121 +164,7 @@ if __name__ == "__main__":
 
     # Read source model into memory
     with open(file_to_inject, 'rb') as f:
-        source_file_data = f.read()
-        #Grab the XML file separately while the file is open
-        f.seek(8)
-        file_entry_name, file_entry_uncompressed_size, file_entry_compressed_size, file_entry_offset, file_entry_flags = struct.unpack("<64sIIII", f.read(64+4+4+4+4))
-        xml_file_info_from_header = [file_entry_offset, file_entry_compressed_size, file_entry_uncompressed_size, file_entry_flags]
-        file_entry_name, file_entry_uncompressed_size, file_entry_compressed_size, file_entry_offset, file_entry_flags = struct.unpack("<64sIIII", f.read(64+4+4+4+4))
-        first_file_after_xml_info_from_header = [file_entry_offset, file_entry_compressed_size, file_entry_uncompressed_size, file_entry_flags] #Also grab info for file 2, for chunking the buffer
-        f.seek(xml_file_info_from_header[0])
-        xml_file = None
-        if xml_file_info_from_header[3] & 1:
-            xml_file = uncompress_nislzss(f, xml_file_info_from_header[2], xml_file_info_from_header[1])
-        if xml_file_info_from_header[3] & 4:
-            xml_file = uncompress_lz4(f, xml_file_info_from_header[2], xml_file_info_from_header[1])
-        if xml_file_info_from_header[3] & 8:
-            xml_file = uncompress_zstd(f, xml_file_info_from_header[2], xml_file_info_from_header[1])
-        patched_file_data = bytearray(source_file_data)
-    
-    # Determine XML compression type
-    xml_compression_type = int.from_bytes(source_file_data[84:88], "little")
-        
-    # For filenames of equal length, no offset patching required
-    if len(target_string) == len(source_string):
-        # Generate the search and substitution strings to inject into the target model
-        if sourcefile.split('_')[-1][0] == 'C':
-            source_string = sourcefile
-            target_string = targetfile
-        else:
-            source_string = sourcefile.split('_')[0] + '_' + sourcefile.split('_')[1]
-            target_string = targetfile.split('_')[0] + '_' + targetfile.split('_')[1]
-        xml_offset = int.from_bytes(source_file_data[80:84], "little")
-        source_string_offset = patched_file_data.find(bytes(source_string.encode()), xml_offset + 1)
-        patched_file_data[source_string_offset:source_string_offset+len(source_string)] = bytes(target_string.encode())
-
-    # If the filenames are of different length, the XML file size will have changed and the file offsets need to change
-    if not len(target_string) == len(source_string) and xml_compression_type == 0:
-        # Generate the search and substitution strings to inject into the target model
-        if sourcefile.split('_')[-1][0] == 'C':
-            source_string = '<asset symbol="' + sourcefile + '">'
-            target_string = '<asset symbol="' + targetfile + '">'
-        else:
-            source_string = '<asset symbol="' + sourcefile.split('_')[0] + '_' + sourcefile.split('_')[1] + '">'
-            target_string = '<asset symbol="' + targetfile.split('_')[0] + '_' + targetfile.split('_')[1] + '">'
-        offset_difference = len(target_string) - len(source_string)
-
-        # Patch model with substitution string
-        patched_file_data = bytearray(source_file_data.replace(bytes(source_string, 'ascii'),bytes(target_string, 'ascii')))
-        total_offsets = source_file_data[4]
-        file_offsets = []
-        # Read in all the current file offsets
-        for offset in range(total_offsets):
-            offset_location = (offset+1)*80
-            file_offsets.append(int.from_bytes(source_file_data[offset_location:offset_location+4], "little"))
-        # Calculate all the new file offsets
-        new_file_offsets = [i + offset_difference for i in file_offsets]
-        new_file_offsets[0] = file_offsets[0] # The first offset doesn't actually change
-        # Set the new file offsets into the patched file data
-        for offset in range(total_offsets):
-            offset_location = (offset+1)*80
-            offset_bytes = new_file_offsets[offset].to_bytes(4, 'little')
-            for intbyte in range(4):
-                patched_file_data[offset_location+intbyte] = offset_bytes[intbyte]
-        # Calculate and set the new XML file size
-        new_xml_size_bytes = (int.from_bytes(source_file_data[72:76], "little") + offset_difference).to_bytes(4, 'little')
-        for intbyte in range(4):
-            patched_file_data[72+intbyte] = new_xml_size_bytes[intbyte]
-            patched_file_data[76+intbyte] = new_xml_size_bytes[intbyte]
-
-    # If the filenames are of different length and the original XML file is compressed, the XML file will need to be replaced.
-    if not len(target_string) == len(source_string) and not xml_compression_type == 0:
-        # Generate the search and substitution strings to inject into the target model
-        if sourcefile.split('_')[-1][0] == 'C':
-            source_string = '<asset symbol="' + sourcefile + '">'
-            target_string = '<asset symbol="' + targetfile + '">'
-        else:
-            source_string = '<asset symbol="' + sourcefile.split('_')[0] + '_' + sourcefile.split('_')[1] + '">'
-            target_string = '<asset symbol="' + targetfile.split('_')[0] + '_' + targetfile.split('_')[1] + '">'
-
-        # Patch uncompressed XML file with substitution string
-        xml_file = bytearray(xml_file.replace(bytes(source_string, 'ascii'),bytes(target_string, 'ascii')))
-
-        # Calculate offset difference for files beyond the XML file
-        offset_difference = len(xml_file) - xml_file_info_from_header[1]
-
-        # Split the .pkg file into header, {XML - not needed}, and file archive.
-        patched_header = patched_file_data[0:xml_file_info_from_header[0]]
-        patched_file_archive = patched_file_data[first_file_after_xml_info_from_header[0]:]
-
-        # Patch the header with all new offsets and new XML file size and compression
-        total_offsets = patched_header[4]
-        file_offsets = []
-        # Read in all the current file offsets
-        for offset in range(total_offsets):
-            offset_location = (offset+1)*80
-            file_offsets.append(int.from_bytes(patched_header[offset_location:offset_location+4], "little"))
-        # Calculate all the new file offsets
-        new_file_offsets = [i + offset_difference for i in file_offsets]
-        new_file_offsets[0] = file_offsets[0] # The first offset doesn't actually change
-        # Set the new file offsets into the patched file data
-        for offset in range(total_offsets):
-            offset_location = (offset+1)*80
-            offset_bytes = new_file_offsets[offset].to_bytes(4, 'little')
-            for intbyte in range(4):
-                patched_header[offset_location+intbyte] = offset_bytes[intbyte]
-        # Calculate and set the new XML file size
-        patched_header[72:76] = len(xml_file).to_bytes(4, 'little') # Uncompressed size
-        patched_header[76:80] = len(xml_file).to_bytes(4, 'little') # Compressed size (no longer compressed)
-        patched_header[84:88] = (0).to_bytes(4, 'little') # Set compression flag to 0
-
-        #Reassemble the .pkg file
-        patched_file_data = patched_header + xml_file + patched_file_archive
+        new_asset_symbol = retrieve_asset_symbol(f)
 
     # Write patched model into target
-    with open(targetfile + '.pkg', 'wb') as f:
-        f.write(patched_file_data)
-
-    # Clean up memory (probably not necessary)
-    del source_file_data
-    del patched_file_data
+    inject_asset_symbol_into_pkg(targetfile + '.pkg.original', targetfile + '.pkg', new_asset_symbol)
